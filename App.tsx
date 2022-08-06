accaf204362d3ab2e11032a7f54dacc53ab6b957
yuds <darcy.u83@icloud.com>
2022-08-06 16:13:55 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import AgoraVideoScrn from './src/screens/AgoraVideoScrn';
+
+interface IProps {}
+
+const App = ({}: IProps) => {
+  return (
+    <View style={{}}>
+      <AgoraVideoScrn />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/screens/AgoraVideoScrn.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+interface IProps {}
+
+const AgoraVideoScrn = ({}: IProps) => {
+  return (
+    <View style={{}}>
+      <Text>AgoraVideoScrn</Text>
+    </View>
+  );
+};
+
+export default AgoraVideoScrn;
